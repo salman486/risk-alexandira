e2d74af7918c60edaae4e0dec8475971b10a0d94
salman486 <msalman2k1@gmail.com>
2022-09-30 18:28:02 +0500

    add pagination in methodology

--- src/components/tabs/Methodology.tsx
+++ src/components/tabs/Methodology.tsx
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import cn from 'classnames';
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 import { useData } from '../../context/data';
+import ReactPaginate from 'react-paginate';
 
 enum MethodologyEnum {
   'opinions',
@@ -19,11 +20,27 @@ const tabs = [
   },
 ];
 
+const itemsPerPage = 5;
+
 const Methodology = () => {
   const [activeMethodologyTab, setActiveMethodologyTab] = useState(
     MethodologyEnum.opinions
   );
   const { data } = useData();
+  const [pageCount, setPageCount] = useState(0);
+  const [itemOffset, setItemOffset] = useState(0);
+
+  const handlePageClick = (event: { selected: number }) => {
+    if (!data?.methodologies) return;
+    const newOffset =
+      (event.selected * itemsPerPage) % data?.methodologies.length;
+    setItemOffset(newOffset);
+  };
+
+  useEffect(() => {
+    if (!data?.methodologies) return;
+    setPageCount(Math.ceil(data?.methodologies.length / itemsPerPage));
+  }, [itemOffset, data]);
 
   return (
     <div className="mt-5 px-5 lg:px-8">
@@ -46,30 +63,35 @@ const Methodology = () => {
       </div>
 
       <div className="mt-8 space-y-5">
-        {data?.methodologies.map(({ Heading, Link, Text }, i) => (
-          <div key={i}>
-            <h2 className="font-semibold text-sky-500">{Heading}</h2>
-            <p className="mt-1.5 text-gray-500">{Text}</p>
+        {data?.methodologies
+          .slice(itemOffset, itemOffset + itemsPerPage)
+          .map(({ Heading, Link, Text }, i) => (
+            <div key={i}>
+              <h2 className="font-semibold text-sky-500">{Heading}</h2>
+              <p className="mt-1.5 text-gray-500">{Text}</p>
 
-            <div className="mt-2 flex items-center space-x-1 text-sm italic">
-              <span className="text-gray-500">Open Article:</span>
-              <a href={Link} className="text-violet-500">
-                {Link}
-              </a>
+              <div className="mt-2 flex items-center space-x-1 text-sm italic">
+                <span className="text-gray-500">Open Article:</span>
+                <a href={Link} className="cur text-violet-500">
+                  {Link}
+                </a>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
 
-      <div className="mx-auto mt-5 flex max-w-md items-center justify-between rounded-md bg-gray-100 px-6 py-2 text-cyan-400 lg:px-10">
-        <FaChevronLeft />
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>4</button>
-        <button>5</button>
-        <FaChevronRight />
-      </div>
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel={<FaChevronRight />}
+        previousLabel={<FaChevronLeft />}
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={pageCount}
+        renderOnZeroPageCount={() => null}
+        activeClassName="font-bold text-lg"
+        className="mx-auto mt-5 flex max-w-md items-center justify-between rounded-md bg-gray-100 px-6 py-2 text-cyan-400 lg:px-10"
+        disabledClassName="!text-gray-400 !cursor-not-allowed"
+      />
     </div>
   );
 };
